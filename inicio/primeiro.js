7b235a79beea4259236691fd582362121a1ad008
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-18 17:10:21 -0300

    comandos e conceitos basicos

--- inicio/primeiro.js
+++ inicio/primeiro.js
@@ -1,1 +1,19 @@
-console.log("legal 2")
+console.log("bom dia!");
+console.log("boa tarde!");
+console.log("boa noite!");
+// console.log('print'), assim que é gerado um comentário, também pode ser usado depois da sentença
+
+
+/*
+    assim é
+    gerado um 
+    comentário
+    com muitas
+    linhas 
+*/
+console.log('')
+{
+    console.log('passo #01');  //isso é um bloco de código, lembra, entre chaves
+    console.log('passo #02');
+    console.log('passo #03');
+}
