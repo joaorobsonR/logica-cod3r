e3ad1d2c1f8c9c8a56f749ad148b5bf65916dae3
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-18 18:01:49 -0300

    apresentados as declarações de variáveis e alguns dados

---
+++ inicio/dados_1.js
@@ -0,0 +1,19 @@
+var nome = 'caneta';
+var preco = 10;
+let imposto = 1.5;
+let precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(preco);
+console.log(imposto);
+console.log('preço final, ', precoFinal);
+
+nome = 'caneta azul'
+console.log(nome)
+
+/*
+    obs: uma variável pode ser criada 
+    sem utilar a palavra reservada var,
+    mas isso pode gerar problemas futuros 
+    que serão explicados mais adiante.
+*/
