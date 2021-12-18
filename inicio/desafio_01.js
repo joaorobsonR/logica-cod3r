3fb8fa3824734ab9e7cc6b2f866a4bb79feaf87d
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-18 17:35:52 -0300

    primeiro desafio muito simples resolvido como propos o professor

---
+++ inicio/desafio_01.js
@@ -0,0 +1,18 @@
+// desafio de senteças e blocos
+
+{
+    console.log('bloco de numeros impares');
+    console.log('numero 1');
+    console.log('numero 3');
+    console.log('numero 5');
+    console.log('numero 7');
+    console.log('numero 9');
+}
+{
+    console.log('bloco de numeros pares');
+    console.log('numero 2');
+    console.log('numero 4');
+    console.log('numero 6');
+    console.log('numero 8');
+    console.log('numero 10');
+}
