b20634a26ff4376d4840e6b8d7581333ff58c75b
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-20 22:29:01 -0300

    desafios para testaros conceitos aprendidos

---
+++ inicio/desafio_area.js
@@ -0,0 +1,8 @@
+const PI = 3.1415;
+let raio = 16;
+let area = PI*raio*raio;
+
+console.log('o valor da area da circunferencia é , '+ area + 'm²');
+
+console.log('a area da circunferencia é, ' + Math.PI*raio*raio + 'm²');
+//nessa senteça foi utilizado a função Math com uma presiçao maior
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,18 @@
+// desafio de troca de valres de variáveis
+
+let a = 7;
+let b = 94;
+let aux;
+console.log(a + ' ' + b);
+
+aux = a;
+a = b;
+b = aux;
+console.log(a + ' ' + b);
+
+
+//teste de troca de valor usado no python
+
+[a, b] = [b, a];
+console.log(a, b);
+// deu certo só faltou colocar os colchetes
