5c059d340ce25edba45fce17234840a75bfc06d6
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-23 11:54:03 -0300

    desafio usando alguns operadores aritimeticos

---
+++ operadores/desafio_op_aritimetico.js
@@ -0,0 +1,23 @@
+//operações literais
+console.log(3+7);
+console.log(3-7);
+console.log(3*7);
+console.log(3/7);
+console.log(3%7);
+
+let a = 15;
+let b = 3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//desafio
+let f = 73;
+let c = 0;
+
+c = (f -32)*5/9
+
+console.log('o valor da conversão de F° em C° é, '+ c);
