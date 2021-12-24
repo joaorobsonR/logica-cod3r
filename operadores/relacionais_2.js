db8ea3f40dc4c126d176c8def0ccba691a7b1430
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-23 23:57:44 -0300

    um pouco sobre tipagem

---
+++ operadores/relacionais_2.js
@@ -0,0 +1,24 @@
+/*
+    tipagem é muito importante
+    para as liguagens de programação
+    e para os programadores, a tipagem
+    é classificada entre fraca e forte, 
+    e isso quer dizer o quanto uma variável
+    pode receber tipos diferentes de dados
+    depois de ter sido declarada.
+*/
+
+let x = 1;
+console.log(x);
+
+x = true
+console.log(x);
+
+x = 'tipagem fraca'
+console.log(x);
+
+/*
+    o linguagem javascript é de tipagem fraca
+    diferente da linguagem Java ou C++ que é
+    de tipagem forte.
+*/
