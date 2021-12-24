2023162319cf2aea98c46d579cecdbf3275c6f07
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-23 23:32:40 -0300

    noçoes de operdores relacionais e tipagem

---
+++ operadores/relacionais_1.js
@@ -0,0 +1,16 @@
+console.log(3 > 7);
+console.log(3 < 7);
+console.log(3 <= 7);
+console.log(3 >= 7);
+console.log(3 != 7);
+console.log(3 == 7);
+
+let a = 15;
+let b = 3;
+
+console.log(a < b);
+console.log(a > b);
+console.log(a <= b);
+console.log(a >= b);
+console.log(a != b);
+console.log(a == b);
