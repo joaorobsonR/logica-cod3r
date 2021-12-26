9d6afddd159c5ad0a7a3657c338db3dd2b8a3641
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-26 19:15:03 -0300

    desafio usando operadores relacionais

---
+++ operadores/desafio_op_relacionais.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = 2;
+
+// 5 expressões com o valor true
+console.log(a < b);
+console.log(a <= b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a + 1 == b);
+
+// 5 expressões com o valor false
+console.log(a > b);
+console.log(a >= b);
+console.log(a == b);
+console.log(a === b);
+console.log(a -1 == b);
