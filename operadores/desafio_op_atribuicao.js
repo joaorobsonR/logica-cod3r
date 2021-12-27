24692af1a2a38547e4257b26c51a2d1b29ec0d82
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-27 17:37:36 -0300

    operadores de atribuiçaõ

---
+++ operadores/desafio_op_atribuicao.js
@@ -0,0 +1,9 @@
+let texto = 'listagem de nomes';
+
+texto += '\n=================\n';
+texto += '\n1° joão  robson';
+texto += '\n2° isis ramalho';
+texto += '\n3° maria odilia';
+texto += '\n4° mia';
+
+console.log(texto)
---
+++ operadores/op_atribuicao.js
@@ -0,0 +1,18 @@
+// no javascript podemos usar os operadores aritimeticos junto com o da atribuiçaõ
+
+let x = 10;
+x += 2;
+x -= 2;
+x *= 2;
+x /= 2;
+x %= 2;
+
+console.log(x);
+
+// tmabém podemos concatenar strings usando essa tecnica
+
+let w = 'texto';
+w += ', mais texo'
+w += ', !!!';
+
+console.log(w);
