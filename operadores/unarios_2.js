7c3c3ca967c3fc42fbc0b9c4e532f81142aa06d0
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-29 22:17:44 -0300

    particularidas do javascript

---
+++ operadores/unarios_2.js
@@ -0,0 +1,10 @@
+console.log(!!true);
+console.log(!!false);
+
+console.log(!!'texto');
+console.log(!!''); //apenas a dupla negaçãi da string vazia retorna false
+
+console.log(!!1);
+console.log(!!12);
+console.log(!!0);
+console.log(!!0.0001); // apenas a dupla negação do 0 retorna false
