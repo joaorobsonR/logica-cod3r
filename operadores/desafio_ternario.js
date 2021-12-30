ab26982fbfefc91ef1e5969987876d37d972c42c
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-30 06:42:53 -0300

    desamfio ternarios, comentário adicionado

--- operadores/desafio_ternario.js
+++ operadores/desafio_ternario.js
@@ -5,3 +5,9 @@ let operacao = '/' ;
 
 let resultado = operacao == '+' ? a + b : operacao == '-' ? a - b : operacao == '*' ? a * b : a / b;
 console.log('o resultado é', resultado); 
+
+/* 
+apesar de ser um programa básico a lógica talvez não
+seja entendida de imediato por alguém que não conhece a 
+liguagem, existem formas mais intuitivas de resolver esse desafio.
+*/
