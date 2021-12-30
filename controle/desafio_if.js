25f2b15ff17d1872761614a0980a71cf555292bc
joaorobsonR <jrobsonramalho@gmail.com>
2021-12-30 10:36:17 -0300

    desafio sobre notas usando apenas a estrutura if

---
+++ controle/desafio_if.js
@@ -0,0 +1,21 @@
+const nota = 7;
+
+if (nota <= 4.5 && nota <= 0) {
+    console.log('F');
+}
+
+if (nota > 4.5 && nota < 5) {
+    console.log('D');
+}
+
+if (nota >= 5 && nota < 7) {
+    console.log('C');
+}
+
+if (nota >= 7 && nota < 9) {
+    console.log('B');
+}
+
+if (nota >= 9 && nota <= 10) {
+    console.log('A');
+}
