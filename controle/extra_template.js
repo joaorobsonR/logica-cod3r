888729dbc6bdf94437bf28e240acb1177d810e8a
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-05 11:48:49 -0300

    template string muito legal e util

---
+++ controle/extra_template.js
@@ -0,0 +1,12 @@
+// template string é uma for mais simples de concatenar variáveis e strings
+
+const a = 1
+const b = 2
+
+console.log('a = ' + a + ', b = ' + b)
+console.log("a =  " + a + ", b = " + b)
+
+console.log(`a = ${a}, b = ${b}`)
+console.log(`O valor randomico é ${Math.random()}`)
+
+//muito mais simples usando o simbolo do crase
