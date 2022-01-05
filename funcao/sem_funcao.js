8045eb38e8f56d429a7f578c481f4d1fc4a0d767
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-05 15:14:10 -0300

    exemplo de codigo sem uso de função

---
+++ funcao/sem_funcao.js
@@ -0,0 +1,17 @@
+const d = 10
+const m = 4
+const a = 2020
+
+console.log(`dia: ${d}`)
+console.log(`mes: ${m}`)
+console.log(`ano: ${a}`)
+
+const d2 = 15
+const m2 = 5
+const a2 = 2020
+
+console.log(`dia: ${d2}`)
+console.log(`mes: ${m2}`)
+console.log(`ano: ${a2}`)
+
+//no proximo exercicio veremos com o uso de função pode melhorar o código, além de relutilizalo
