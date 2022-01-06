c17877e8770e78ac90620e5eb9729e095a79fb64
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-05 22:53:15 -0300

    primeiro desafio sobre funções

---
+++ funcao/desafio_funcao_1.js
@@ -0,0 +1,14 @@
+function somar(x, y) {
+    let resultado = x + y
+    console.log(resultado)
+}
+
+function subtrair(x, y) {
+    console.log(`${x - y}`)
+}
+
+somar(3, 4)
+somar(31, 98)
+
+subtrair(11, 7)
+subtrair(19, 52)
