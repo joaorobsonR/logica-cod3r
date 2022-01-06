42814954467b3f98e769738baa5823caca7bd6a2
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-06 11:19:20 -0300

    função sem retorno

---
+++ funcao/funcao_e_undefined.js
@@ -0,0 +1,12 @@
+/*
+nessa aula foi visto função que retorna um valor undefined
+*/
+
+function semRetorno() {
+    console.log('função foi chamada')
+}
+
+let a = 7
+let b = a + 3
+
+console.log(a , b, semRetorno()) //quando é tentado mostrar o retorna da função o resultado é undefined
