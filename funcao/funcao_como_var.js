de2589a536aa363490c7e186aea843c25bf77891
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-06 15:52:01 -0300

    funçaõ armazenada em uma constante

---
+++ funcao/funcao_como_var.js
@@ -0,0 +1,16 @@
+/*
+nessa aula foi visto como armazenar uma função em uma variavel
+uma tecnica que muitas linguagens não aceitão, mas que no 
+futuro em uma situação será muito util
+*/
+
+const x = 10
+const y = 11
+
+const somar = function (a, b) { //funçaõ sem nome
+    return a + b
+}
+
+console.log(somar(x, y))
+
+//isso poderia ser aplicada para todas as funções anteriores
