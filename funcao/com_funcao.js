613387fd6aacc4e05db380c4711215c86fbc6d70
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-05 21:11:40 -0300

    exemplo de funçao par melhor o código

---
+++ funcao/com_funcao.js
@@ -0,0 +1,16 @@
+const d = 10
+const m = 4
+const a = 2020
+
+function exibirData1() {
+    console.log(`dia: ${d}`)
+    console.log(`mes: ${m}`)
+    console.log(`ano: ${a}`)
+}
+
+function exibirData2() {
+    console.log(`${d}/${m}/${a}`)
+}
+
+exibirData1()
+exibirData2()
