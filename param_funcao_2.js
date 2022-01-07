ca60484011ff26570be40dac3770024611ea6a7b
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-07 11:57:42 -0300

    passando valor padrão para parametros

---
+++ param_funcao_2.js
@@ -0,0 +1,11 @@
+function somar(a=0, b=0, c=0, d=0) {
+    return a + b + c + d
+}
+
+console.log(1, 2, 3, 4, 5, 6)
+console.log(1, 2, 3, 4)
+console.log(1, 2, 3)
+console.log(1, 2)
+console.log(1)
+
+//isso resolve o problema caso não é passodo o numero de parametro determinado
