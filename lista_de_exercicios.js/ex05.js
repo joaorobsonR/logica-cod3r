862d75c1845f2e49fc78f72d99d039f2bf364820
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-11 23:17:51 -0300

    exercicio de função usando controle i

---
+++ lista_de_exercicios.js/ex05.js
@@ -0,0 +1,11 @@
+function analisaMaior(a, b) {
+    if (a >= b ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(analisaMaior(15, 10))
+console.log(analisaMaior(10, 15))
+console.log(analisaMaior(10, 10))
